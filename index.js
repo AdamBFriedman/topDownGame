1f28997ff2130855331ee7ac1a76df15f8a17345
adambfriedman <adam.friedman5@gmail.com>
2022-12-19 20:28:08 -0600

    Draw game map.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const canvas = document.querySelector('canvas');
 canvas.width = 1024;
 canvas.height = 576;
+
+const c = canvas.getContext('2d');
+
+c.fillStyle = 'white';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+const map = new Image();
+map.src = './images/Game Map.png';
+map.onload = () => {
+  c.drawImage(map, 0, 0);
+};
