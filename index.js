42d6f52f62c3cb75dee16631d07bd77d8d4c1eab
adambfriedman <adam.friedman5@gmail.com>
2022-12-20 18:39:29 -0600

    Render player on screen.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const canvas = document.querySelector('canvas');
-canvas.width = 1024;
-canvas.height = 576;
-
 const c = canvas.getContext('2d');
+
+canvas.width = 1700;
+canvas.height = 780;
 
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const map = new Image();
-map.src = './images/Game Map.png';
-map.onload = () => {
-  c.drawImage(map, 0, 0);
+const mapImage = new Image();
+mapImage.src = './images/Game Map.png';
+
+const playerImage = new Image();
+playerImage.src = './images/playerDown.png';
+
+mapImage.onload = () => {
+  c.drawImage(mapImage, 0, -150);
+  c.drawImage(playerImage, canvas.width / 2, canvas.height / 2);
 };
